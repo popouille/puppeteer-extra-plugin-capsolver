2e83b1f630e40b162b509494e1412f3d4627d723
qwertyy _ <moneyalien666@protonmail.com>
2022-11-18 22:33:58 +0100

    [plugin] rebranding

--- src/index.js
+++ src/index.js
@@ -1,40 +1,40 @@
 'use strict'
 const { PuppeteerExtraPlugin } = require('puppeteer-extra-plugin');
-const Captchaai = require('captchaai-npm');
+const CapSolver = require('capsolver-npm');
 const { createCursor } = require("ghost-cursor");
 const axios = require("axios");
 
 const sleep = ms => new Promise(r => setTimeout(r, ms));
 
 /**
- * Captchaai.io Puppeteer Plugin:
- * Integrates captchaai-npm (api wrapper) & adds extra captcha DOM features
+ * capsolver.com Puppeteer Plugin:
+ * Integrates capsolver-npm (api wrapper) & adds extra captcha DOM features
  */
-class CaptchaaiPlugin extends PuppeteerExtraPlugin {
+class CapSolverPlugin extends PuppeteerExtraPlugin {
     constructor(opts = { }) { super(opts); }
 
-    get name() { return 'captchaai-plugin'; }
+    get name() { return 'capsolver-plugin'; }
 
     get defaults() { return { }; }
 
     /** Initialize **/
-    async onPluginRegistered(){ if(this.opts.handler && this.opts.handler.verbose !== 0) { console.log('[CaptchaaiPlugin] Plugin registered'); } }
+    async onPluginRegistered(){ if(this.opts.handler && this.opts.handler.verbose !== 0) { console.log('[CapSolverPlugin] Plugin registered'); } }
 
     /**
      * @param {Target} target - PuppeteerTarget
      */
-    async onTargetChanged(target) { if(this.opts.handler && this.opts.handler.verbose === 2) { console.log('[CaptchaaiPlugin][Target changed]['+JSON.stringify(target)+']'); } }
+    async onTargetChanged(target) { if(this.opts.handler && this.opts.handler.verbose === 2) { console.log('[CapSolverPlugin][Target changed]['+JSON.stringify(target)+']'); } }
 
     /**
-     * Set new Captchaai.io API task handler instance.
+     * Set new capsolver.com API task handler instance.
      *
-     * @param {string} apikey - personal apikey in captchaai.io API
+     * @param {string} apikey - personal apikey in capsolver.com API
      * @param {number} verbose - (0/1/2) verbose level for task outputs
      */
-    setHandler(apikey, verbose=0){ this.opts.handler = new Captchaai(apikey, verbose, 500); this.opts.verbose = verbose; return this; }
+    setHandler(apikey, verbose=0){ this.opts.handler = new CapSolver(apikey, verbose, 500); this.opts.verbose = verbose; return this; }
 
     /**
-     * Returns Captchaai.io API task handler instance.
+     * Returns capsolver.com API task handler instance.
      *
      */
     handler(){ return this.opts.handler; }
@@ -46,7 +46,7 @@ class CaptchaaiPlugin extends PuppeteerExtraPlugin {
      * @param {boolean} retry - Retry if failed
      */
     async hcaptchaclicker(page, retry=true){
-        if(this.opts.handler === undefined) throw 'No existing handler for CaptchaaiPlugin. Please, set it and add your apiKey with: `CaptchaaiPlugin.setHandler("apikey");`.'
+        if(this.opts.handler === undefined) throw 'No existing handler for CapSolverPlugin. Please, set it and add your apiKey with: `CapSolverPlugin.setHandler("apikey");`.'
 
         const self = this;
 
@@ -60,8 +60,8 @@ class CaptchaaiPlugin extends PuppeteerExtraPlugin {
             await cursor.click('iframe');
             await page.waitForTimeout(2500);
             frame = await page.frames().find(x => x.url().includes("https://newassets.hcaptcha.com"));
-            if(frame === undefined){ if(self.opts.handler.verbose !== 0){ console.log('[CaptchaaiPlugin][Triggering...]'); } await trigger(); }
-            if(self.opts.handler.verbose !== 0){ console.log('[CaptchaaiPlugin][Triggered]'); }
+            if(frame === undefined){ if(self.opts.handler.verbose !== 0){ console.log('[CapSolverPlugin][Triggering...]'); } await trigger(); }
+            if(self.opts.handler.verbose !== 0){ console.log('[CapSolverPlugin][Triggered]'); }
         }
 
         async function solve(){
@@ -73,7 +73,7 @@ class CaptchaaiPlugin extends PuppeteerExtraPlugin {
             await frame.click('.language-selector');
 
             const [lang] = await frame.$x("//span[contains(text(), 'English')]");
-            if (lang) { await lang.click().catch(e => console.log('[CaptchaaiPlugin][Could not change translate to english! Original question assigned. ]')); }
+            if (lang) { await lang.click().catch(e => console.log('[CapSolverPlugin][Could not change translate to english! Original question assigned. ]')); }
 
             const qElement = await frame.$('.prompt-text');
             const question = await frame.evaluate(el => el.textContent, qElement);
@@ -91,26 +91,26 @@ class CaptchaaiPlugin extends PuppeteerExtraPlugin {
                 });
             }
 
-            // handle captchaai.io image classification service - receiving boolean array
-            let captchaai = await self.opts.handler.hcaptchaclassification(question, base64, false);
+            // handle capsolver.com image classification service - receiving boolean array
+            let capsolver = await self.opts.handler.hcaptchaclassification(question, base64, false);
             await frame.waitForTimeout(200);
             let coords;
 
-            if(parseInt(captchaai.error) === 0){
-                coords = captchaai.apiResponse.solution.objects;
+            if(parseInt(capsolver.error) === 0){
+                coords = capsolver.apiResponse.solution.objects;
             }else{
-                if(captchaai.apiResponse.errorId !== 0){ throw ('Captchaai API invalid task: '+JSON.stringify(captchaai.apiResponse)); }
-                else{ coords = captchaai.apiResponse.solution.objects; }
+                if(capsolver.apiResponse.errorId !== 0){ throw ('capsolver API invalid task: '+JSON.stringify(capsolver.apiResponse)); }
+                else{ coords = capsolver.apiResponse.solution.objects; }
             }
 
-            for (const [i, isImage] of coords.entries()) { if(isImage) { await frame.click(`div.task-image:nth-child(${i + 1})`); if(self.opts.handler.verbose !== 0){ console.log('[CaptchaaiPlugin][Clicked! '+(i+1)+']'); } } await sleep(Math.floor(Math.random() * (600 - 100) + 100))}
+            for (const [i, isImage] of coords.entries()) { if(isImage) { await frame.click(`div.task-image:nth-child(${i + 1})`); if(self.opts.handler.verbose !== 0){ console.log('[CapSolverPlugin][Clicked! '+(i+1)+']'); } } await sleep(Math.floor(Math.random() * (600 - 100) + 100))}
             await frame.waitForTimeout(200);
             let submitbtn = await frame.$('.button-submit.button');
             await submitbtn.evaluate(b => { b.click(); })
             await page.waitForTimeout(1200);
 
             const tryagain = await frame.$eval('.display-error', e => { return e.getAttribute('aria-hidden') });   // false = Try again prompt displayed
-            if(tryagain === "false"){ if(retry === true){ if(self.opts.handler.verbose !== 0){ console.log('[CaptchaaiPlugin][Re-trying...]'); } await solve(); }else{ throw('Try again'); } }
+            if(tryagain === "false"){ if(retry === true){ if(self.opts.handler.verbose !== 0){ console.log('[CapSolverPlugin][Re-trying...]'); } await solve(); }else{ throw('Try again'); } }
         }
 
         try{
@@ -120,7 +120,7 @@ class CaptchaaiPlugin extends PuppeteerExtraPlugin {
                 await frame.waitForSelector('div.hcaptcha-logo').catch(e => { throw(1); }).then(e =>{ });
                 await solve();
                 if(second === true) {
-                    if(self.opts.handler.verbose !== 0){ console.log('[CaptchaaiPlugin][2-step HCaptcha detected]'); }
+                    if(self.opts.handler.verbose !== 0){ console.log('[CapSolverPlugin][2-step HCaptcha detected]'); }
                     await frame.waitForTimeout(200);
                     await solve();
                 }
@@ -131,6 +131,6 @@ class CaptchaaiPlugin extends PuppeteerExtraPlugin {
 
 }
 
-/** Default export, CaptchaaiPlugin */
-const defaultExport = opts => new CaptchaaiPlugin(opts);
+/** Default export, CapSolverPlugin */
+const defaultExport = opts => new CapSolverPlugin(opts);
 module.exports = defaultExport;
